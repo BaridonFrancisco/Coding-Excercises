f78c24966ae0aacc14416a623762f17f71b97348
BaridonFrancisco <baridonfrancisco@gmail.com>
2023-08-26 23:51:44 -0300

    commit09

---
+++ codewarsChallenges/src/Kyu5/SimplePigLatin.java
@@ -0,0 +1,67 @@
+package Kyu5;
+
+
+
+
+/*Move the first letter of each word to the end of it,
+ then add "ay" to the end of the word. Leave punctuation marks untouched.*/
+public class SimplePigLatin {
+    public static void main(String[] args) {
+
+        System.out.println(pigLatin("Pig latin is cool"));
+        System.out.println(pigLatin("Hello world !"));
+        System.out.println(pigLatin("This is my string"));
+        System.out.println(pigLatin("Oqe[]"));
+    }
+    public static String pigLatin(String str){
+        var arr=str.split(" ");
+        StringBuilder aux=new StringBuilder("");
+        String first;
+        int distancia=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].matches("^[A-Za-z ]+$")){
+                if(i!=0){
+                    distancia=aux.length()-1;
+                    aux.append(arr[i]);
+                    aux.deleteCharAt(distancia+1);
+                    first=arr[i].substring(0,1);
+                    aux.append(first).append("ay");
+                    if (i != arr.length - 1) {
+                        aux.append(" ");
+                    }
+
+                }else{
+                    aux.append(arr[i]);
+                    first=arr[i].substring(0,1);
+                    aux.deleteCharAt(distancia);
+                    aux.append(first).append("ay ");
+                }
+
+            }else{
+                aux.append(arr[i]);
+            }
+
+        }
+        return String.valueOf(aux).trim();
+    }
+
+    public static String solucion2(String str){
+
+        String[] words = str.split("\\s+");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            if (word.matches("\\w+")) {
+                String transformedWord = word.replaceFirst("(\\w)(\\w*)", "$2$1ay");
+                result.append(transformedWord).append(" ");
+            } else {
+                result.append(word);
+            }
+        }
+
+        return result.toString().trim();
+
+    }
+
+
+}
--- codewarsChallenges/src/Kyu6/Count_the_smiley_faces.java
+++ codewarsChallenges/src/Kyu6/Count_the_smiley_faces.java
@@ -1,17 +1,53 @@
 package Kyu6;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Count_the_smiley_faces {
     public static void main(String[] args) {
+        List<String> smileys=new ArrayList<>();
+        loadSmileys(smileys,":)",":D",":-}",":-()");
+        System.out.println(countSmileys(smileys));
+        List<String> smileys2=new ArrayList<>();
+        loadSmileys(smileys2,":)","XD",":0}","x:-","):-","D:");
+        System.out.println(countSmileys(smileys2));
+        List<String> smileys3=new ArrayList<>();
+        loadSmileys(smileys3,":)",":D","X-}","xo)",":X",":-3",":3");
+        System.out.println(countSmileys(smileys3));
+        List<String> smileys4=new ArrayList<>();
+        loadSmileys(smileys4,":)",":)","x-]",":ox",";-(",";-)",";~(",":~D");
+        System.out.println(countSmileys(smileys4));
 
     }
-    public static int countSmileys(String [] arr){
-        String obligatoriosNarizBoca=":;D)";
-        for (String s:arr){
-            if(s.startsWith(":")||s.startsWith(";")){
+    public static int countSmileys(List<String>arr){
+        if(arr.size()==0){
+            return 0;
+        }
+        int count=0;
+        String aux;
+        boolean condicion;
+        for(int i=0;i<arr.size();i++){
+            aux= arr.get(i);
+            condicion=(aux.startsWith(":")||aux.startsWith(";"))
+                    && (aux.endsWith("D") || aux.endsWith(")"));
+            if(aux.length()>=2 && aux.length()<4){
+                if(aux.length()==2){
+                    if(condicion){
+                        count++;
+                    }
+                }else{
+                    if(condicion && ((aux.charAt(1) == '~') || (aux.charAt(1)=='-'))){
+                        count++;
+                    }
+                }
 
             }
         }
-        return 0;
+        return count;
+    }
+    public static void loadSmileys(List<String>arr,String... smileys){
+        arr.addAll(Arrays.asList(smileys));
     }
 }
 //Exercises
